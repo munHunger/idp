40c19d3ed79e657a6bc63e750b97bc63548a2358
Adonai Tesfay <adonai.tesfay@cybercom.com>
2018-02-08 12:02:09 +0100

    IDP-1 Changed inheritance from Exception to Throwable

--- src/main/java/se/munhunger/idp/model/ErrorMessage.java
+++ src/main/java/se/munhunger/idp/model/ErrorMessage.java
@@ -7,7 +7,7 @@ import io.swagger.annotations.ApiModelProperty;
  * @author Marcus Münger
  */
 @ApiModel(value = "An error message describing something that went wrong")
-public class ErrorMessage extends Exception {
+public class ErrorMessage extends Throwable {
     @ApiModelProperty(value = "An overview of what went wrong")
     public String message;
     @ApiModelProperty(value = "The details of what went wrong")
