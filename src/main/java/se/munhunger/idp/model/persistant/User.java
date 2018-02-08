d0e78886f30e10380b3285cedd732f98eaf5aa05
Adonai Tesfay <adonai.tesfay@cybercom.com>
2018-02-08 18:03:41 +0100

    IDP-1 Refactoring of User exist validation

--- src/main/java/se/munhunger/idp/model/persistant/User.java
+++ src/main/java/se/munhunger/idp/model/persistant/User.java
@@ -87,15 +87,5 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "username='" + username + '\'' +
-                ", hashPassword='" + hashPassword + '\'' +
-                ", firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
+    
 }
