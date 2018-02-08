7174f10c093acb22f3b0bb023ab3a3e957a42b57
Adonai Tesfay <adonai.tesfay@cybercom.com>
2018-02-08 17:24:39 +0100

    IDP-1 Refactoring on EmailValidation

--- src/main/java/se/munhunger/idp/Util/EmailValidation.java
+++ src/main/java/se/munhunger/idp/Util/EmailValidation.java
@@ -1,15 +1,11 @@
 package se.munhunger.idp.Util;
 
 import org.apache.commons.validator.routines.EmailValidator;
+import se.munhunger.idp.exception.EmailNotValidException;
 
 public class EmailValidation {
     public static boolean isValidEmailAddress(String email) {
         boolean valid = false;
-        try {
-            valid = EmailValidator.getInstance(true).isValid(email);
-        } catch (Exception e){
-            e.printStackTrace();
-    }
-        return valid;
+        return valid = EmailValidator.getInstance(true).isValid(email);
     }
 }
