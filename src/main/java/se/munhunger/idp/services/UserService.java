3a1fa82e4ab25993c5e80edf4231cbc340a0e3c5
Adonai Tesfay <adonai.tesfay@cybercom.com>
2018-02-19 08:38:23 +0100

    Refactoring logging ClientService

--- src/main/java/se/munhunger/idp/services/UserService.java
+++ src/main/java/se/munhunger/idp/services/UserService.java
@@ -25,37 +25,40 @@ public class UserService {
     private UserDAO userDAO;
 
     public void createUser(User user) throws NoSuchAlgorithmException, EmailNotValidException {
-        log.info("Creating User");
+        log.info(() -> "Creating User: " + user.toString());
         user.setHashPassword(HashPass.hashPassword(user.getHashPassword()));
         if (!EmailValidation.isValidEmailAddress(user.getEmail())) {
-            log.error("Error could not create User due to invalid email: " + user.getEmail());
+            log.error(() -> "Error could not create User due to invalid email: " + user.getEmail());
             throw new EmailNotValidException();
         }
         userDAO.createUser(user);
-        log.info("Creating User Successful with User: " + user.toString());
+        log.info(() -> "Creating User Successful with User: " + user.toString());
     }
 
     public User getUser(String username) throws UserNotInDatabaseException {
-        log.info("Getting user: " + username);
+        log.info(() -> "Getting user: " + username);
         return userDAO.getUser(username).orElseThrow(UserNotInDatabaseException::new);
     }
 
     public void updateUser(User user) throws NoSuchAlgorithmException, UserNotInDatabaseException, EmailNotValidException {
-        log.info("Updating user: " + user.getUsername());
+        log.info(() -> "Updating user: " + user.getUsername());
         user.setHashPassword(HashPass.hashPassword(user.getHashPassword()));
-        if (!EmailValidation.isValidEmailAddress(user.getEmail()))
+        if (!EmailValidation.isValidEmailAddress(user.getEmail())) {
+            log.error(() -> "Error could not create User due to invalid email: " + user.getEmail());
             throw new EmailNotValidException();
+        }
         userDAO.updateUser(user);
-        log.info("Updating user: " + user.getUsername() + " Successful");
+        log.info(() -> "Updating user: " + user.getUsername() + " Successful");
     }
 
     public void deleteUser(String username) throws UserNotInDatabaseException, OrphanageException {
-        log.info("Deleting user: " + username);
+        log.info(() -> "Deleting user: " + username);
         User user = getUser(username);
         if (user.getClients().size() != 0){
+            log.error(() -> "Error could not create User due to OrphanException: " + user.getEmail());
             throw new OrphanageException();
         }
         userDAO.deleteUser(username);
-        log.info("Deleting user: " + username + " Successful");
+        log.info(() -> "Deleting user: " + username + " Successful");
     }
 }
